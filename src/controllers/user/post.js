911ece31e9a7d5a7d69c89a4ed4ae25a6a73c3b6
Victor F <fernandes.victor@hotmail.com>
2022-02-15 23:35:07 -0300

    feat(user): removed console.log from user post

--- src/controllers/user/post.js
+++ src/controllers/user/post.js
@@ -2,10 +2,7 @@ exports.path = "/user";
 exports.method = "post";
 exports.middlewares = [];
 exports.handler = models => async (req, res) => {
-
     const { name, email, password } = req.body;
     const user = await models.repository.User.createUser({ name, email, password });
-    console.log(user.name);
-    console.log(user.email);
     return res.status(200).json(user)
 };
