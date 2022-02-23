e8852c09f081fe3fe51ce6bc2df134e535d9ee17
Victor F <fernandes.victor@hotmail.com>
2022-02-22 21:25:33 -0300

    feat(test): commit to test the husky script

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
 const config = require('../config/database');
 
 const db = {};
