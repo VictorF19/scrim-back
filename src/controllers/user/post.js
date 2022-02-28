eb503fcdde662377082681ee2481707449750f82
Victor F <fernandes.victor@hotmail.com>
2022-02-28 16:21:46 -0300

    feat(package.json): Added new packages to the JWT Auth

--- src/controllers/user/post.js
+++ src/controllers/user/post.js
@@ -2,11 +2,13 @@ exports.path = '/user';
 exports.method = 'post';
 exports.middlewares = [];
 exports.handler = (models) => async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, nickname } = req.body;
+  // add verification if the user already exists
   const user = await models.repository.User.createUser({
     name,
     email,
     password,
+    nickname,
   });
   return res.status(200).json(user);
 };
