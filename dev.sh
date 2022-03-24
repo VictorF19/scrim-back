15d046dc5771e2b46b31599f248f9da782d1193d
erickk474 <erickk474@gmail.com>
2022-03-24 11:43:22 -0300

    script para levantamento de ambiente dev

---
+++ dev.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+isPassword="0"
+isPort="0"
+password=""
+port=""
+
+for line in $(echo $(<.env) | tr "=" "\n")
+do
+    if [ $isPassword == "1" ];
+    then
+        password=$line
+        isPassword="0"
+    fi
+    if [ $isPort == "1" ];
+    then
+        port=$line
+        isPort="0"
+    fi
+    if [ $line == "DATABASE_PASSWORD" ];
+    then
+        isPassword="1"
+    fi
+    if [ $line == "DATABASE_PORT" ];
+    then
+        isPort="1"
+    fi
+done
+
+docker run -d \
+    --name scrim \
+    -e POSTGRES_PASSWORD=$password \
+    -p $port:port \
+    -e PGDATA=/var/lib/postgresql/data/pgdata \
+    -v scrim-postgres:/var/lib/postgresql/data \
+    postgres
+
+docker exec -it scrim psql -U postgres -c 'CREATE DATABASE scrim;'
+
+npx sequelize-cli db:migrate
+
+npm run start
