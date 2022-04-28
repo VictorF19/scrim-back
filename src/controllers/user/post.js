448af80b34e69190395ed34c4d7fd20c929356b4
Victor F <fernandes.victor@hotmail.com>
2022-04-27 23:22:41 -0300

    feat(PR): made changes requested at PR
    - validation of the nickname
    - destructing on body values

--- src/controllers/user/post.js
+++ src/controllers/user/post.js
@@ -6,24 +6,27 @@ exports.path = '/user';
 exports.method = 'post';
 exports.authenticate = false;
 exports.middlewares = [validateUserBody, validateErrorBody];
-exports.handler = (models) => async (req, res) => {
-  const { name, email, password, nickname } = req.body;
+exports.handler =
+  (models) => async ({ body: { name, email, password, nickname } }, res) => {
+    if (await models.repository.User.checkUserExists(email)) {
+      return res.status(400).json({
+        message: 'An user with this e-mail already exists',
+      });
+    }
 
-  if (await models.repository.User.checkUserExists(email)) {
-    return res
-      .status(400)
-      .json({ message: 'An user with this e-mail already exists' });
-  }
+    const user = await models.repository.User.createUser({
+      name,
+      email,
+      password,
+      nickname,
+    });
 
-  const user = await models.repository.User.createUser({
-    name,
-    email,
-    password,
-    nickname,
-  });
-
-  return res.status(201).json({
-    user: { name: user.name, email: user.email, nickname: user.nickname },
-    token: generateToken({ id: user.id }),
-  });
-};
+    return res.status(201).json({
+      user: {
+        name: user.name,
+        email: user.email,
+        nickname: user.nickname,
+      },
+      token: generateToken({ id: user.id }),
+    });
+  };
